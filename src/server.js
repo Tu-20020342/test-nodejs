8ccc4c23c07bf1ba20b26b1a8ac2efd4b0ee1a94
Tu-20020342 <20020342@vnu.edu.vn>
2023-05-07 16:50:50 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/a', (req, res) => {
+    res.send('Hello a!')
+})
+
+app.get('/bbb', (req, res) => {
+    res.send('<h1>Hello b!</h1>')
+  })
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
